74a69d2a790468d155d026688db69263d0060778
Halimazeez <Halim.azeez@googlemail.com>
2017-12-29 23:44:12 +0000

    Finished styling mostly, mobile first

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 ### Front-end Web Development
 ### p15180243 | Halim Azeez
 
-Live Website: `https://halimazeez.github.io/Web/`
+Live Website: [https://halimazeez.github.io/Web/]
+
+
+Repo: [https://github.com/Halimazeez/Web]
+
 
 `git clone https://github.com/Halimazeez/Web`
+
+##Full java script `is syntactically valid`
+Source: [http://esprima.org/demo/validate.html]
+
+
+##Full `CSS Validated`
+Source: [https://jigsaw.w3.org/css-validator/validator#css]
+> Only contains 2 errors but is due to the 'svg' tag's arnt CSS properties.
+
+
+##Full `HTML5 Validated`
+Source:[https://validator.w3.org/nu/]
+> Contains a few irrelevant errors, which are not respectively appropriate to the
+nature of the web design, e.g. sections are used for styling not content.
+
+
+#Sources:
+
+[https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav]
+[https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show]
+[https://developers.google.com/maps/documentation/javascript/adding-a-google-map]
+[https://developers.google.com/maps/documentation/javascript/examples/event-simple]
+[https://images.google.com]
+[https://stackoverflow.com/questions/26707886/easiest-way-to-fill-array-with-an-object-in-javascript-without-coding-a-loop]
+[https://stackoverflow.com/questions/12458456/how-to-change-the-zoom-level-of-google-maps-programmatically]
--- js.js
+++ js.js
@@ -1,3 +1,58 @@
+//event handlers for heading
+document.getElementById("puregymtab").addEventListener("click",function(event) {
+    headerloop(headerpure); //pass document id as parameter
+});
+document.getElementById("dwtab").addEventListener("click",function(event) {
+    headerloop(headerdw); //pass document id as parameter
+});
+document.getElementById("thegymtab").addEventListener("click",function(event) {
+    headerloop(headerthe); //pass document id as parameter
+});
+document.getElementById("anytimetab").addEventListener("click",function(event) {
+    headerloop(headerany); //pass document id as parameter
+});
+
+// vars to hold document id's (used later for array populate)
+var headerpure = document.getElementById("contentpure");
+var headerdw = document.getElementById("contentdw");
+var headerthe = document.getElementById("contentthegym");
+var headerany = document.getElementById("contentanytime")
+
+//similar to marker, takes passed parameter 'x' and changes style to 'block' from
+//'none' to display the header on screen.
+function headerloop(x) {
+
+  var y = document.getElementById("gymcontent"); //set y to gymcontent section id
+
+  //populate array constant with header ids
+  const set = [headerpure, headerdw, headerthe, headerany];
+
+  //*im quite proud of this*
+  // set pointer of i to 0 => 1st element of array
+  //for loop bounds: [0-3] = 4 elements of the array
+  for (var i=0; i < set.length; i++) {
+    var a = set[i]; //set a to the current iteration of set element
+    if (a.style.display = "block") { //if the header is displayed ->
+      a.style.display = "none" // disable the display
+    }
+    x.style.display = "block" //show header respective to the passed parameter x.
+  }
+} //end headerloop
+
+//footer repeats on sitemap for headerloop function above
+document.getElementById("puregymtab2").addEventListener("click", function(event) {
+  headerloop(headerpure); //pass document id as parameter
+});
+document.getElementById("dwtab2").addEventListener("click", function(event) {
+  headerloop(headerdw); //pass document id as parameter
+});
+document.getElementById("thegymtab2").addEventListener("click", function(event) {
+  headerloop(headerthe); //pass document id as parameter
+});
+document.getElementById("anytimetab2").addEventListener("click", function(event) {
+  headerloop(headerany); //pass document id as parameter
+});
+
 // google maps
 
 var dmu = {lat: 52.629827, lng: -1.139331}; //dmu location
@@ -82,60 +137,4 @@ google.maps.event.addDomListener(window, "resize", function() {
   var center = map.getCenter();
   google.maps.event.trigger(map, "resize");
   map.setCenter(center);
-});
-
-
-//event handlers for heading
-document.getElementById("puregymtab").addEventListener("click",function(event) {
-    headerloop(headerpure); //pass document id as parameter
-});
-document.getElementById("dwtab").addEventListener("click",function(event) {
-    headerloop(headerdw); //pass document id as parameter
-});
-document.getElementById("thegymtab").addEventListener("click",function(event) {
-    headerloop(headerthe); //pass document id as parameter
-});
-document.getElementById("anytimetab").addEventListener("click",function(event) {
-    headerloop(headerany); //pass document id as parameter
-});
-
-// vars to hold document id's (used later for array populate)
-var headerpure = document.getElementById("contentpure");
-var headerdw = document.getElementById("contentdw");
-var headerthe = document.getElementById("contentthegym");
-var headerany = document.getElementById("contentanytime")
-
-//similar to marker, takes passed parameter 'x' and changes style to 'block' from
-//'none' to display the header on screen.
-function headerloop(x) {
-
-  var y = document.getElementById("gymcontent"); //set y to gymcontent section id
-
-  //populate array constant with header ids
-  const set = [headerpure, headerdw, headerthe, headerany];
-
-  //*im quite proud of this*
-  // set pointer of i to 0 => 1st element of array
-  //for loop bounds: 0-3 = 4 elements of the array
-  for (var i=0; i < set.length; i++) { 
-    var a = set[i]; //set a to the current iteration of set element
-    if (a.style.display = "block") { //if the header is displayed ->
-      a.style.display = "none" // disable the display
-    }
-    x.style.display = "block" //show header respective to the passed parameter x.
-  }
-} //end headerloop
-
-//footer repeats on sitemap for headerloop function above
-document.getElementById("puregymtab2").addEventListener("click", function(event) {
-  headerloop(headerpure); //pass document id as parameter
-});
-document.getElementById("dwtab2").addEventListener("click", function(event) {
-  headerloop(headerdw); //pass document id as parameter
-});
-document.getElementById("thegymtab2").addEventListener("click", function(event) {
-  headerloop(headerthe); //pass document id as parameter
-});
-document.getElementById("anytimetab2").addEventListener("click", function(event) {
-  headerloop(headerany); //pass document id as parameter
 });
