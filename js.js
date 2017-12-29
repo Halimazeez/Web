329d73f7cc6e964abc5965c85792455371aa409e
Halim Azeez <Halim@Halims-MacBook-Pro.local>
2017-12-29 04:29:14 +0000

    added google map api

--- js.js
+++ js.js
@@ -1,11 +1,33 @@
-
 // google maps
-var map;
 
-//init for gmaps to point at dmu at default
+
+//init google maps
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 46.980252, lng: 4.987793},
-    zoom: 8
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: dmu,
+    scrollwheel: true,
+    disableDefaultUI: true
   });
-}
+
+  // plot dmu on map as default
+  var marker = new google.maps.Marker({
+    position: dmu,
+    map: map,
+    animation:google.maps.Animation.DROP
+  });
+
+
+//init map on website load (Default is located @ DMU)
+google.maps.event.addDomListener(window, "load", initMap());
+
+//responsive centering of map plot for resize (simple get and set method)
+google.maps.event.addDomListener(window, "resize", function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
+});
+
+/*function responsivecenter() { // old
+  center = map.getCenter();
+}*/
